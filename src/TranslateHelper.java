5fc8b3a38c0f736d949235685422af77adae91ea
Harold Bartolo <habartolom@unal.edu.co>
2022-11-26 21:35:53 -0500

    Declaracion inicial clase

--- src/TranslateHelper.java
+++ src/TranslateHelper.java
@@ -1,2 +1,100 @@
 public class TranslateHelper {
+    public static String getTypescriptModifier(JavaGrammarParser.ModifierContext ctx){
+
+        String modifier = "";
+
+        if(ctx.classOrInterfaceModifier() != null)
+            modifier = getTypeScriptClassOrInterfaceModifier(ctx.classOrInterfaceModifier());
+
+        return modifier;
+    }
+
+    public static String getTypeScriptClassOrInterfaceModifier(JavaGrammarParser.ClassOrInterfaceModifierContext ctx){
+
+        String typeScriptModifier = "";
+
+        if(ctx.PUBLIC() != null)
+            typeScriptModifier = ctx.PUBLIC().getText();
+
+        else if(ctx.PROTECTED() != null)
+            typeScriptModifier = ctx.PROTECTED().getText();
+
+        else if(ctx.PRIVATE() != null)
+            typeScriptModifier = ctx.PRIVATE().getText();
+
+        return typeScriptModifier;
+    }
+
+    public static String getTypeScriptMemberDeclaration(JavaGrammarParser.MemberDeclarationContext ctx){
+        String memberDeclaration = "";
+
+        if(ctx.methodDeclaration() != null)
+            memberDeclaration = getTypeScriptMethodDeclaration(ctx.methodDeclaration());
+
+        return memberDeclaration;
+    }
+
+    public static String getTypeScriptMethodDeclaration(JavaGrammarParser.MethodDeclarationContext ctx){
+        String methodDeclaration = "";
+
+        methodDeclaration += getTypeScriptIdentifier(ctx.identifier()) + " : ";
+        methodDeclaration += getTypeScriptTypeTypeOrVoid(ctx.typeTypeOrVoid());
+
+
+        return methodDeclaration;
+    }
+
+    public static String getTypeScriptTypeTypeOrVoid(JavaGrammarParser.TypeTypeOrVoidContext ctx){
+        String typeOrVoid = "void";
+
+        if (ctx.typeType() != null)
+            typeOrVoid = getTypeScriptTypeType(ctx.typeType());
+
+        return  typeOrVoid;
+    }
+
+    public static String getTypeScriptTypeType(JavaGrammarParser.TypeTypeContext ctx){
+        String typeType = "";
+
+        if(ctx.primitiveType() != null)
+            typeType +=  getTypeScriptPrimitiveType(ctx.primitiveType());
+
+        if(ctx.classOrInterfaceType() != null)
+            typeType += getTypeScriptClassOrInterfaceType(ctx.classOrInterfaceType());
+
+        return typeType;
+    }
+
+    public static String getTypeScriptClassOrInterfaceType(JavaGrammarParser.ClassOrInterfaceTypeContext ctx){
+        String classOrInterfaceType = "";
+
+        if(ctx.identifier().size() > 0){
+            classOrInterfaceType += getTypeScriptIdentifier(ctx.identifier().get(0));
+        }
+        return classOrInterfaceType;
+    }
+
+    public static String getTypeScriptIdentifier(JavaGrammarParser.IdentifierContext ctx){
+        String identifier = "";
+
+        if(ctx.IDENTIFIER() != null)
+            identifier = ctx.IDENTIFIER().getText();
+
+        return  identifier;
+    }
+
+    public static String getTypeScriptPrimitiveType(JavaGrammarParser.PrimitiveTypeContext ctx){
+        String primitiveType = "";
+
+        if(ctx.BOOLEAN() != null)
+            primitiveType = ctx.BOOLEAN().getText();
+
+        else if(ctx.CHAR() != null)
+            primitiveType = "string";
+
+        else
+            primitiveType = "number";
+
+        return primitiveType;
+    }
 }
--- src/TranslatorListener.java
+++ src/TranslatorListener.java
@@ -1,13 +1,27 @@
 public class TranslatorListener extends JavaGrammarBaseListener {
 
+    @Override public void enterClassBodyDeclaration(JavaGrammarParser.ClassBodyDeclarationContext ctx) {
+        String classBodyDeclaration = "";
+
+        if(ctx.modifier().size() > 0){
+            classBodyDeclaration += TranslateHelper.getTypescriptModifier(ctx.modifier().get(0)) + " ";
+        }
+
+        if(ctx.memberDeclaration() != null){
+            classBodyDeclaration += TranslateHelper.getTypeScriptMemberDeclaration(ctx.memberDeclaration());
+        }
+
+        System.out.println(classBodyDeclaration);
+    }
 
     @Override
     public void enterTypeDeclaration(JavaGrammarParser.TypeDeclarationContext ctx) {
+
         String example = "";
 
         if(ctx.classDeclaration() != null){
             example += "class ";
-            String nameClass = ctx.classDeclaration().identifier().getText();
+            String nameClass = TranslateHelper.getTypeScriptIdentifier(ctx.classDeclaration().identifier());
             example += nameClass;
 
             System.out.println(example);
@@ -15,7 +29,7 @@ public class TranslatorListener extends JavaGrammarBaseListener {
 
         if (ctx.interfaceDeclaration() != null) {
             example += "interface ";
-            String nameClass = ctx.interfaceDeclaration().identifier().getText();
+            String nameClass = TranslateHelper.getTypeScriptIdentifier(ctx.classDeclaration().identifier());
             example += nameClass;
 
             System.out.println(example);
